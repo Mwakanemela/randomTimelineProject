8c5e7ff8ebbd0d29c2b894054fc849e10e9f7685
Mwakanemela Kayange - Coming from the commandline <kayangemwakanemela@gmail.com>
2023-01-16 09:41:21 +0300

    Setting up the structure

---
+++ app.js
@@ -0,0 +1,7 @@
+const timelineIndicator = document.querySelector('.timelineIndicator');
+const eventInfo = document.querySelector('.eventInfo');
+const page = {indicators:[], info:[]};
+
+function generateEvent() {
+    
+}
